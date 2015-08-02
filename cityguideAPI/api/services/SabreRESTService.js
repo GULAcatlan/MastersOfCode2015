aa183909c5448c49f577386ddc58ea403dd79314
Sier Violencia <sierisimo@gmail.com>
2015-08-02 03:27:53 -0500

    Pre-test

--- cityguideAPI/api/services/SabreRESTService.js
+++ cityguideAPI/api/services/SabreRESTService.js
@@ -11,16 +11,22 @@ module.exports = {
       }
     };
 
+    //It's just a get :D
     https.get(requestOptions, function(response) {
+      //Ok... we can recive a lot of data on various chuncks
       var fullChunck = "";
       response.on('data', function(chunck) {
         var mobileResponse = {};
 
         fullChunck += chunck.toString();
 
-        try{
+        try {
           var jsonFinal = JSON.parse(fullChunck);
 
+          delete jsonFinal.OriginLocation;
+          delete jsonFinal.LoopBackWeeks;
+          delete jsonFinal.Links;
+          
           if (jsonFinal.hasOwnProperty('errorCode')) {
             mobileResponse.status = {
               code: 404,
@@ -35,14 +41,23 @@ module.exports = {
           }
 
           res.jsonx(mobileResponse);
-        }catch(e){
-          console.log("Not all data has arrived");
+        } catch (e) {
+
         }
-//        res.jsonx(mobileResponse);
       });
     });
   },
   topflights: function(req, res) {
-
+    var datePlus3 = new Date(new Date().getTime() + (3 * 24 * 60 * 60 * 1000)), //get a date with 3 days in the future
+      datePlus8 = new Date(new Date() + (8 * 24 * 60 * 60 * 1000)); // get a date with a 8 days in the future
+    dateFlight = datePlus3.getFullYear() + "-" + (datePlus3.getMonth() + 1 < 10 ? "0" + (datePlus3.getMonth() + 1) : datePlus3.getMonth() + 1) + "-" + (datePlus3.getDate() < 10 ? "0" + datePlus3.getDate() : datePlus3.getDate()),
+    dateReturn = datePlus3.getFullYear() + "-" + (datePlus3.getMonth() + 1 < 10 ? "0" + (datePlus3.getMonth() + 1) : datePlus3.getMonth() + 1) + "-" + (datePlus3.getDate() < 10 ? "0" + datePlus3.getDate() : datePlus3.getDate()),
+      requestOptions = {
+        host: urlSabre,
+        path: "/v1/shop/flights/fares?origin=" + req.query.origin + "&topdestinations=5&loopbackweeks=2",
+        headers: {
+          Authorization: "Bearer " + sails.config.restAuth.access_token
+        }
+      };
   },
 }
